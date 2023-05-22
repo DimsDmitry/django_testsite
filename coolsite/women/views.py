0c3a95c5f9918059770c3fa67e85ccf3548efc94
dmitriy <dmitriy250300@mail.ru>
2023-05-22 16:23:14 +0300

    22.05.2023

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import *
 
 
@@ -16,7 +16,10 @@ def categories(request, catid):
 
 def archive(request, year):
     if int(year) > 2020:
-        raise Http404()
+        return redirect('home', permanent=True)
+        # генерирует 302 если False, permanent=True - 303
+        # указываем имя home и определяем его. Если указать "/", то адрес будет определён явно,
+        # и при смене шаблона придётся везде менять ссылку
     return HttpResponse(f"<h1>Архив по годам </h1><p>{year}</p>")
 
 
