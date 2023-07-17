b1285eb549d9ae48b5c3a6dfc5b6346dc18ff5c0
dmitriy <dmitriy250300@mail.ru>
2023-07-17 16:34:44 +0300

    17.07.2023

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -1,4 +1,7 @@
+from django.conf.urls.static import static
 from django.urls import path, re_path
+
+from coolsite import settings
 from .views import *
 
 
@@ -11,3 +14,7 @@ urlpatterns = [
 #http://127.0.0.1:8000/women/ - префикс появится, потому что мы указывали его ещё в coolsite/urls.py
 #Далее он будет добавляться автоматически
 #если добавить сюда ещё маршрут
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = pageNotFound
